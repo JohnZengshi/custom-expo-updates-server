5a440fa5ceae1c24564638a10cfab0eb2433d63b
john <johnzengshi@gmail.com>
2024-05-30 13:54:29 +0800

    🐎 ci:

--- expo-updates-server/build.deploy.test.sh
+++ expo-updates-server/build.deploy.test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+###
+ # @LastEditors: John
+ # @Date: 2024-05-29 14:31:10
+ # @LastEditTime: 2024-05-29 15:54:18
+ # @Author: John
+### 
 # 检测是否存在 neer-updates-server-test 镜像
 if [[ "$(docker images -q neer-updates-server-test 2> /dev/null)" == "" ]]; then
     # 如果不存在，执行 docker build
     echo "neer-updates-server-test 镜像不存在，开始构建..."
-    docker build -t neer-updates-server-test -f Dockerfile-test .
+    docker build -t neer-updates-server-test -f Dockerfile.test .
 fi
 # 检测是否存在 neer-updates-server-test 容器
 if [[ "$(docker ps -aqf name=neer-updates-server-test 2> /dev/null)" != "" ]]; then
@@ -14,4 +20,4 @@ if [[ "$(docker ps -aqf name=neer-updates-server-test 2> /dev/null)" != "" ]]; t
 fi
 # 运行容器
 echo "启动 neer-updates-server-test 容器..."
-docker run -p 3001:3000 -d --name neer-updates-server-test -v /data/server/neer-updates-server/updates-test:/app/updates neer-updates-server-test
+docker run -p 3001:3000 -d --name neer-updates-server-test -v /data/server/neer-updates-server/updates.test:/app/updates neer-updates-server-test
--- expo-updates-server/next.config.js
+++ expo-updates-server/next.config.js
@@ -1,6 +1,13 @@
+/*
+ * @LastEditors: John
+ * @Date: 2024-03-23 15:48:10
+ * @LastEditTime: 2024-05-29 16:03:08
+ * @Author: John
+ */
 // next.config.js
 
 module.exports = {
-  basePath: process.env.BASE_PATH || '', // 设置根路径
+  basePath: process.env.BASE_PATH || "", // 设置根路径
   // 可选：配置其他选项
-}
+  swcMinify: false,
+};
